4873e39e284b9d8ff71bdacbd23b9fcb17a2f467
Andre Van Der Merwe <andre@andrevdm.com>
2018-03-21 17:35:55 +0200

    15: sections started

--- 15_memory/src/Memory.hs
+++ 15_memory/src/Memory.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 
+{-! SECTION< 15_exports !-}
 module Memory ( Memory
               , empty
               , remember
@@ -11,19 +12,25 @@ module Memory ( Memory
               , toList
               , fromList
               ) where
+{-! SECTION> 15_exports !-}
 
 import Protolude hiding (Map, empty, toList)
+{-! SECTION< 15_mem_type !-}
 import qualified Data.Map.Strict as Map
 import           Data.Map.Strict (Map)
 
 -- | Store (remembers) values for a given period (measured by ticks)
 newtype Memory a = Memory (Map Text (Map a Int)) 
+{-! SECTION> 15_mem_type !-}
 
 
+{-! SECTION< 15_empty !-}
 empty :: Memory a
 empty = Memory Map.empty
+{-! SECTION> 15_empty !-}
 
 
+{-! SECTION< 15_remember !-}
 remember :: (Ord a) => (Int -> Int -> Int) -> Text -> Int -> a -> Memory a -> Memory a
 remember combineFn key ttl val (Memory m) =
   Memory $ Map.alter ins key m
@@ -31,11 +38,14 @@ remember combineFn key ttl val (Memory m) =
   where
     ins Nothing = Just $ Map.singleton val ttl
     ins (Just vs) = Just $ Map.insertWith combineFn val ttl vs
+{-! SECTION> 15_remember !-}
 
 
+{-! SECTION< 15_recall !-}
 recall :: Text -> Memory a -> Map a Int
 recall key (Memory m) =
   fromMaybe Map.empty $ Map.lookup key m 
+{-! SECTION> 15_recall !-}
 
 
 forgetAll :: Text -> Memory a -> Memory a
@@ -57,6 +67,7 @@ remembers key val m =
   Map.member val (recall key m)
 
 
+{-! SECTION< 15_tick !-}
 tick :: (Ord a) => Memory a -> Memory a
 tick (Memory m) =
   Memory $ tickBucket <$> m
@@ -69,6 +80,7 @@ tick (Memory m) =
     tickVal :: Maybe Int -> Maybe Int
     tickVal Nothing = Nothing
     tickVal (Just i) = if i - 1 <= 0 then Nothing else Just (i - 1)
+{-! SECTION> 15_tick !-}
     
 
 toList :: Memory a -> [(Text, [(a, Int)])]
